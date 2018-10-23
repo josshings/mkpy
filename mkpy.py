efb1ddedaefeca47ba39e3dc13c796fa6d957f45
josshings <44147561+josshings@users.noreply.github.com>
2018-10-22 21:06:14 -0300

    Create mkpy.py

---
+++ mkpy.py
@@ -0,0 +1,24 @@
+import os, sys
+
+def createFile(fname):
+    with open(fname + ".py", "w") as pyFile:
+        pyFile.write("#! /usr/bin/env python3\n")
+    
+    os.system(f"chmod +x {fname}.py")
+
+try:
+    fname = str(sys.argv[1])
+except IndexError:
+    print("Usage: ./mkpy.py <name of script>")
+else:
+    if os.path.exists(fname + ".py"):
+        ans = input(f"{fname}.py exists, overwrite? [y/n]: ")
+        
+        if ans == "y":
+            os.remove(f"{fname}.py")
+            createFile(fname)
+        else:
+            print("Exiting...")
+            sys.exit(1)
+    else:
+        createFile(fname)
